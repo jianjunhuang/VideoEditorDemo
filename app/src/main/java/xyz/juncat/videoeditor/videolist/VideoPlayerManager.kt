b16b862a9c6258e29d15d0f97369f2c26a68f393
jun <demon903732218@gmail.com>
2021-09-01 18:30:53 +0800

    add TODO

--- app/src/main/java/xyz/juncat/videoeditor/videolist/VideoPlayerManager.kt
+++ app/src/main/java/xyz/juncat/videoeditor/videolist/VideoPlayerManager.kt
@@ -1,9 +1,11 @@
 package xyz.juncat.videoeditor.videolist
 
-import xyz.juncat.videoeditor.SimplePlayer
 import java.lang.ref.WeakReference
 import java.util.concurrent.CopyOnWriteArrayList
 
+/**
+ * delay player.release() to prevent ui freezing
+ */
 object VideoPlayerManager {
 
     private val pendingReleaseQueue = CopyOnWriteArrayList<WeakReference<VideoItemView>>()
@@ -12,6 +14,7 @@ object VideoPlayerManager {
         pendingReleaseQueue.add(WeakReference(player))
     }
 
+    //TODO: Optimization: stop clear when user scroll again
     fun clear() {
         pendingReleaseQueue.forEach {
             it.get()?.stop()
